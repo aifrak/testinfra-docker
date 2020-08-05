a0b4f989edcb01ce9bd14351aa6ffe89f470eb13
aifrak <aifrak@users.noreply.github.com>
2020-08-05 20:28:49 +0200

    Move tests out of main dockerfile (#2)
    - start only sut service
    - move tests out of main dockerfile
    - remove tests on pip and pytest
    - improve code
    - renaming

--- test/build_test.py
+++ test/build_test.py
@@ -9,12 +9,13 @@ def test_python():
     assert platform.python_version() == '3.8.5'
 
 
-def test_pip_packages(host):
+@pytest.mark.parametrize('name,version', [
+    ('testinfra', '5.2.2'),
+])
+def test_pip_packages(host, name, version):
     pip_packages = host.pip_package.get_packages
 
-    assert pip_packages()['pip']['version'] == '20.1.1'
-    assert pip_packages()['pytest']['version'] == '5.4.3'
-    assert pip_packages()['testinfra']['version'] == '5.2.2'
+    assert pip_packages()[name]['version'] == version
 
 
 def test_current_user(host):
